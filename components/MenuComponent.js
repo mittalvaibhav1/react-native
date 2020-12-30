ab1ec61449fc5a3fc2ded0283c0e144c2464b228
mittalvaibhav1 <mittalvaibhavandroid@gmail.com>
2020-12-30 23:46:03 +0530

    FlatList , ListItem

---
+++ components/MenuComponent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View , FlatList } from 'react-native';
+import { Avatar, ListItem } from 'react-native-elements';
+
+function Menu(props) {
+
+    const renderMenuItem = ({ item }) => {
+        return (
+            <ListItem bottomDivider>
+                <Avatar source = { require('./images/uthappizza.png') }/>
+                <ListItem.Content>
+                    <ListItem.Title>{ item.name }</ListItem.Title>
+                    <ListItem.Subtitle>{ item.description }</ListItem.Subtitle>
+                </ListItem.Content>
+            </ListItem>    
+        );
+    };
+    return ( 
+        <FlatList 
+            data = {props.dishes}
+            renderItem = {renderMenuItem}
+            keyExtractor = {item => item.id.toString()}
+        />
+    );
+}
+
+
+export default Menu;
